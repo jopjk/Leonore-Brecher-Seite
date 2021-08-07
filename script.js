ab55aab042d339660a50718a1287a77688639ec9
jopjk <joram907@gmail.com>
2021-08-07 16:52:59 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+/*var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var imgs = document.getElementsByClassName("imgs");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  console.log(slideIndex)
+  console.log(slides)
+  slides[slideIndex-1].style.display = "block";
+  captionText.innerHTML = imgs[slideIndex-1].alt;
+}
+*/
+//navbar
+
+function openNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+window.addEventListener('scroll', throttle(parallax, 14));
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+};
+
+function parallax() {
+  var scrolled = window.pageYOffset;
+  var parallax = document.querySelector(".parallax");
+  // You can adjust the 0.4 to change the speed
+	var coords = (scrolled * 0.4) + 'px'
+    
+  parallax.style.transform = 'translateY(' + coords + ')';
+};
